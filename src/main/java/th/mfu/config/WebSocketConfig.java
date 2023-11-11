aed2ecbd0057387bcaf6c553179a5d7023374c5c
Ratcanon Suwatsiriphol <6531503070@lamduan.mfu.ac.th>
2023-11-12 06:34:02 +0700

    asdasdasd

--- src/main/java/th/mfu/config/WebSocketConfig.java
+++ src/main/java/th/mfu/config/WebSocketConfig.java
@@ -1,10 +1,18 @@
 package th.mfu.config;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import org.springframework.http.server.ServerHttpRequest;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,6 +25,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins(".").withSockJS();
+        registry.addEndpoint("/websocket")
+            .setAllowedOrigins("https://lab-cd-cmqp2bahxa-uc.a.run.app")
+            .withSockJS();
     }
 }
