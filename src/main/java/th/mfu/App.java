c1386d8f1b5a8aa4ce857cacf68364e25758cf26
Ratcanon Suwatsiriphol <6531503070@lamduan.mfu.ac.th>
2023-11-13 03:18:26 +0700

    testws2

--- src/main/java/th/mfu/App.java
+++ src/main/java/th/mfu/App.java
@@ -2,8 +2,12 @@ package th.mfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
+@EnableWebSocket
+@EnableWebSocketMessageBroker
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
