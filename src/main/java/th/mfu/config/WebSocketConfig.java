2f33a605b071c18f5061b61fcf3a4e652e2c040d
Ratcanon Suwatsiriphol <6531503070@lamduan.mfu.ac.th>
2023-11-13 03:54:37 +0700

    testws4

--- src/main/java/th/mfu/config/WebSocketConfig.java
+++ src/main/java/th/mfu/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -18,5 +19,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Bean
     public MyHandler myHandler() {
         return new MyHandler();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
     }
 }
