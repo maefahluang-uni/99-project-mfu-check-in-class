9c0927ec341420f9b9dbeabdc860163fc4c117af
Ratchanon Suwatsiriphol <6531503070@lamduan.mfu.ac.th>
2023-11-17 07:57:25 +0700

    date-sync

--- src/main/java/th/mfu/controller/G_Controller.java
+++ src/main/java/th/mfu/controller/G_Controller.java
@@ -301,15 +301,21 @@ public class G_Controller {
     @GetMapping("/course")
     public String CoursePage(Model model, HttpServletResponse response, HttpServletRequest request) {
         User Myself = (User) request.getAttribute("userdata");
-        if (Myself.getRole() == "STUDENT" || Myself.getRole() == "LECTURER") {
+        if (Myself.getRole() == "STUDENT") {
             List<CourseSection> CourseCollection = CourseSectionRepo.findByStudentID(Myself.getID()); // it's a clone instance not effect direct to real entity
             for (CourseSection v0 : CourseCollection) { // prevent leak password on User Entity
                 for (Student v1 : v0.student) { v1.setPassword("FORBIDDEN"); }
-                for (Lecturer v2 : v0.lecturer) { v2.setPassword("FORBIDDEN"); }
+            }
+            model.addAttribute("mycourse", CourseCollection);
+        } else if (Myself.getRole() == "LECTURER") {
+            List<CourseSection> CourseCollection = CourseSectionRepo.findByLecturerID(Myself.getID()); // it's a clone instance not effect direct to real entity
+            System.out.println(CourseCollection.toString());
+            for (CourseSection v0 : CourseCollection) { // prevent leak password on User Entity
+                for (Lecturer v1 : v0.lecturer) { v1.setPassword("FORBIDDEN"); }
             }
             model.addAttribute("mycourse", CourseCollection);
         }
-        return "Course";
+        return "Lec-Course";
     }
 
     @GetMapping("/contact")
