513a0c377fcead38b5705b0cea022d4768f8d961
6531503074 <6531503074@lamduan.mfu.ac.th>
2023-11-24 09:28:06 +0700

    section

--- src/main/resources/static/js/managecourse.js
+++ src/main/resources/static/js/managecourse.js
@@ -1,81 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     var addCourseModal = new bootstrap.Modal(document.getElementById('addCourseModal'));
-
-//     document.getElementById('addCourseBtn').addEventListener('click', function () {
-//         addCourseModal.show();
-//     });
-
-// document.getElementById('addCourseForm').addEventListener('submit', function (event) {
-//     event.preventDefault();
-
-//     var courseNameInput = document.getElementById('courseName');
-//     var courseName = document.getElementById('courseName').value.trim();
-
-//     if (courseName !== '') {
-
-//         var duplicate = Array.from(document.querySelectorAll('#datatable tbody tr td:nth-child(2)'))
-//             .map(td => td.textContent.trim().toLowerCase())
-//             .includes(courseName.toLowerCase());
-
-//         if (!duplicate) {
-//             console.log('Adding course to the database...');
-
-//             $.ajax({
-//                 url: '/add-course',
-//                 method: 'POST',
-//                 data: { courseName: courseName },
-//                 success: function (response) {
-
-//                     updateCourseTable(response);
-                    
-   
-//                     addCourseModal.hide();
-//                     alert('Add Course succeed.');
-//                     courseNameInput.value = '';
-//                 },
-//                 error: function (error) {
-//                     console.error('Error adding course:', error);
-
-//                     alert('Course with the same name already exists.');
-//                     courseNameInput.value = '';
-//                 }
-//             });
-//         } else {
-
-//             alert('Course with the same name already exists.');
-//             courseNameInput.value = '';
-//         }
-//     } else {
-
-//         alert('Please enter a course name.');
-//     }
-// });
-
-
-// function updateCourseTable(response) {
-
-//     var dataTable = $('#datatable').DataTable();
-
-//     dataTable.clear();
-
-//     response.courses.forEach(function (course) {
-//         dataTable.row.add([
-//             course.id,
-//             course.name,
-//             '<a class="btnEdit" href="#"><i class="fas fa-pencil-alt" title="Edit"></i></a>' +
-//             '<a class="btnDelete" th:href="@{/delete-course/{courseId}(courseId=${course.getID()})}"><i class="fas fa-trash-alt" title="Delete"></i></a>'
-//         ]).draw(false);
-//     });
-//     }
-//     addCourseModal._element.addEventListener('hidden.bs.modal', function () {
-//         document.getElementById('courseName').value = '';
-//     });
-// });
-
-
-// new
-
 document.addEventListener('DOMContentLoaded', function () {
 
     var addCourseModal = new bootstrap.Modal(document.getElementById('addCourseModal'));
@@ -125,24 +47,20 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Event delegation for delete button
     $('#datatable').on('click', '.btnDelete', function (e) {
         e.preventDefault();
     
-        // Show a confirmation dialog
         var confirmation = confirm("Are you sure you want to delete this course?");
     
         if (confirmation) {
             var row = $(this).closest('tr');
             var courseId = row.find('td:first-child').text();
     
-            // Make an AJAX call to delete the course
             $.ajax({
                 url: '/delete-course/' + courseId,
                 method: 'GET',
                 success: function (response) {
                     if (response.success) {
-                        // Use DataTable API to remove the row
                         dataTable.row(row).remove().draw(false);
                         alert('Delete Course succeed.');
                         updateCourseTable(response);
@@ -159,8 +77,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function updateCourseTable(response) {
-        // Clear the input field
         document.getElementById('courseName').value = '';
+        document.getElementById('editCourseName').value = '';
         dataTable.clear();
     
         // Add new rows without clearing existing ones
@@ -168,16 +86,82 @@ document.addEventListener('DOMContentLoaded', function () {
             dataTable.row.add([
                 course.id,
                 course.name,
-                '<a class="btnEdit" href="#"><i class="fas fa-pencil-alt" title="Edit"></i></a>' +
+                '<a class="btnSEC" th:href="@{/manage-course-section/{courseId}(courseId=${course.getID()})}" data-course-id="' + course.id + '"><i class="fas fa-book" title="Section"></i></a>' +
+                '<a class="btnEdit" href="#" data-course-id="' + course.id + '"><i class="fas fa-pencil-alt" title="Edit"></i></a>' +
                 '<a class="btnDelete" href="#" data-course-id="' + course.id + '"><i class="fas fa-trash-alt" title="Delete"></i></a>'
             ]);
         });
     
-        // Redraw the DataTable
         dataTable.draw(false);
     }
 
     addCourseModal._element.addEventListener('hidden.bs.modal', function () {
         document.getElementById('courseName').value = '';
     });
+    $('#datatable').on('click', '.btnEdit', function (e) {
+        e.preventDefault();
+        var row = $(this).closest('tr');
+        var courseId = row.find('td:first-child').text();
+        var currentCourseName = row.find('td:nth-child(2)').text();
+
+        showEditModal(courseId, currentCourseName);
+    });
+
+    $('#datatable').on('click', '.btnSEC', function (e) {
+        e.preventDefault();
+        var row = $(this).closest('tr');
+        var courseId = row.find('td:first-child').text();
+        var currentCourseName = row.find('td:nth-child(2)').text();
+
+        window.location.href = '/manage-course-section/' + courseId;
+    });
+
+
+    function showEditModal(courseId, currentCourseName) {
+        var editCourseModal = new bootstrap.Modal(document.getElementById('editCourseModal'));
+        var editCourseForm = document.getElementById('editCourseForm');
+        var editCourseNameInput = document.getElementById('editCourseName');
+
+        if (!editCourseForm || !editCourseNameInput) {
+            console.error('Required elements not found.');
+            return;
+        }
+
+        editCourseNameInput.value = currentCourseName;
+
+        editCourseModal.show();
+
+        editCourseForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            var editedCourseName = editCourseNameInput.value.trim();
+
+                $.ajax({
+                    url: '/update-course/' + courseId,
+                    method: 'POST',
+                    data: { editedCourseName: editedCourseName },
+                    success: function (response) {
+                        if (response.success) {
+                            editCourseModal.hide();
+                            alert('Edit Course succeed.');
+                            console.log(response.message);
+                            updateCourseTable(response);
+                            location.reload();
+                        } else {
+                            alert(response.message);
+                        }
+                    },
+                    error: function (error) {
+                        console.log('Error');
+                        alert('Course name already exists.');
+                    }
+                });
+        });
+
+        document.getElementById('cancelEditCourseBtn').addEventListener('click', function () {
+            editCourseModal.hide();
+            location.reload();
+        });
+    }
+    
 });
