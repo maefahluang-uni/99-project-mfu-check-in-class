b0d1b40005a57c581295461b21b1653ffd60a0c9
6531503074 <6531503074@lamduan.mfu.ac.th>
2023-11-25 01:56:34 +0700

    admin lecturer

--- src/main/java/th/mfu/repository/LecturerRepository.java
+++ src/main/java/th/mfu/repository/LecturerRepository.java
@@ -1,11 +1,21 @@
 package th.mfu.repository;
 
 import java.util.List;
-import th.mfu.model.*;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import th.mfu.model.Course;
+import th.mfu.model.CourseSection;
+import th.mfu.model.Lecturer;
+import th.mfu.repository.CourseRepository;
 
 public interface LecturerRepository extends CrudRepository<Lecturer, Long> {
+    
     public List<Lecturer> findAll();
     public Lecturer findByID(Long iD);
+    
+    @Query("SELECT COUNT(l) > 0 FROM Lecturer l WHERE LOWER(l.Name) = LOWER(:name)")
+    boolean existsByNameIgnoreCase(@Param("name") String name);
 }
